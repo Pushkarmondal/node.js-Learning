fffabf0002266cd87d5766ff58a0f0ae713a5b15
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2024-02-23 23:38:33 +0530

    Add files via upload

---
+++ REPL.js
@@ -0,0 +1,15 @@
+/* 
+   node.js tutorial starts here. 
+   -----------------------------
+   Lesson 1 :-
+   -----------
+   REPL:
+        `command to enter REPL - "node".``
+    
+    (i) js expression
+    (ii) use variables
+    (iii) multilines codes
+    (iv) use (_) to get last results
+    (v) we can use editor mode
+*/
+
